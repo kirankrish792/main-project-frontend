6ec1f10ff5061260d6fa23298ffdd4c6ac9bbe0b
Nandu Krishna T <nandukrishnat3@gmail.com>
2023-03-24 20:20:47 +0530

    working dashborad

--- src/routes/dashboard.tsx
+++ src/routes/dashboard.tsx
@@ -1,10 +1,11 @@
 import axios from "axios";
-import { Component, createSignal, For, onMount } from "solid-js"
+import { Component, createSignal, For, onMount, Show, Switch } from "solid-js";
+import { Form } from "solid-start/data/Form";
 import { useContractData, useUserData } from "~/store";
 
-import type { ContractData,JsonInterface } from "~/types"
+import type { ContractData, JsonInterface } from "~/types";
 
-const [address, setAddress] = createSignal("")
+// const [address, setAddress] = createSignal("")
 const [values, setValues] = createSignal<string[]>([]);
 const [output, setOutput] = createSignal("")
 
@@ -13,14 +14,143 @@ export default function Dashboard() {
 
   const { contractDetails } = useContractData()
 
+  // const [contractDetails, setContractDetails] = createSignal({
+  //   "address": "0xE5d90332D5CFFb58c022fC89A7C75DB2106a8e2c",
+  //   "jsonInterface": [
+  //     {
+  //       "inputs": [
+  //         {
+  //           "internalType": "address",
+  //           "name": "",
+  //           "type": "address"
+  //         }
+  //       ],
+  //       "name": "balances",
+  //       "outputs": [
+  //         {
+  //           "internalType": "uint256",
+  //           "name": "totalBalance",
+  //           "type": "uint256"
+  //         },
+  //         {
+  //           "internalType": "uint256",
+  //           "name": "numDeposits",
+  //           "type": "uint256"
+  //         },
+  //         {
+  //           "internalType": "uint256",
+  //           "name": "numWithdrawals",
+  //           "type": "uint256"
+  //         }
+  //       ],
+  //       "stateMutability": "view",
+  //       "type": "function",
+  //       "constant": true,
+  //       "signature": "0x27e235e3"
+  //     },
+  //     {
+  //       "inputs": [],
+  //       "name": "depositeMoney",
+  //       "outputs": [],
+  //       "stateMutability": "payable",
+  //       "type": "function",
+  //       "payable": true,
+  //       "signature": "0x3f925f7a"
+  //     },
+  //     {
+  //       "inputs": [
+  //         {
+  //           "internalType": "address payable",
+  //           "name": "_to",
+  //           "type": "address"
+  //         },
+  //         {
+  //           "internalType": "uint256",
+  //           "name": "_amount",
+  //           "type": "uint256"
+  //         }
+  //       ],
+  //       "name": "withdrawMoney",
+  //       "outputs": [],
+  //       "stateMutability": "nonpayable",
+  //       "type": "function",
+  //       "signature": "0xf274c897"
+  //     }
+  //   ]
+  // })
+
+  let inputElements: NodeListOf<HTMLInputElement>;
+
+  onMount(() => {
+    inputElements = document.querySelectorAll("input[type='text']")
+  })
+
+  const Button: Component<JsonInterface> = (props) => {
+    const handleClick = (e) => {
+      const obj: {
+        method: string,
+        address: string,
+        params: string[]
+      } = {
+        method: [e.target.name] as unknown as string,
+        address: "",
+        params: []
+      };
+      obj.method = e.target.name
+      obj.address = account()
+      inputElements.forEach((el) => {
+        if (el.name == e.target.name) {
+          obj.params.push(el.value)
+          el.value = ""
+        }
+      })
+      handleGet(obj)
+    }
+
+    return (
+      <div>
+        <button onClick={handleClick} name={props.name}>{props.name}</button>
+        <Show when={props.inputs.length > 0}>
+          <For each={props.inputs}>
+            {
+              (el, index) => (
+                <>
+                  <br />
+                  <label>
+                    {el.name}
+                  </label>
+                  <input type="text" name={props.name} placeholder={el.type} onInput={(e) => handleChange(index(), e)} />
+                </>
+              )
+            }
+          </For>
+        </Show>
+      </div>
+    )
+  }
+
+
+  const setterFunctions = contractDetails().jsonInterface.filter((el) => el.type == "function" && ["nonpayable", "payable"].includes(el.stateMutability))
+  const getterFunctions = contractDetails().jsonInterface.filter((el) => el.type == "function" && ["view"].includes(el.stateMutability))
+
+
+  // let categories = contractDetails().jsonInterface.reduce((pre, curr) => ({ ...pre, [curr.name]: [] }), {})
+
   return (
     <>
       <h2>Contract Address : {contractDetails().address}</h2>
       <hr />
       {account}
-      <div>Address : <input type="text" onInput={(e) => setAddress(e.currentTarget.value)} value={address()} /></div>
+      {/* <div>Address : <input type="text" onInput={(e) => setAddress(e.currentTarget.value)} value={address()} /></div> */}
       <h3>Functions</h3>
-      <For each={contractDetails().jsonInterface}>
+      <h4>Getter Function</h4>
+      <For each={getterFunctions}>
+        {
+          (item) => (<Button {...item} />)
+        }
+      </For>
+      <h4>Setter Function</h4>
+      <For each={setterFunctions}>
         {
           (item) => (<Button {...item} />)
         }
@@ -33,28 +163,6 @@ export default function Dashboard() {
   )
 }
 
-const Button: Component<JsonInterface> = (props) => {
-  if (props.inputs.length > 0) {
-    return (
-      <div>
-        <button>{props.name}</button>
-        <For each={props.inputs}>
-          {
-            (el, index) => (
-              <>
-                <input type="text" placeholder={[el.type, el.name].join(" ")} onInput={(e) => handleChange(index(), e)} />
-              </>
-            )
-          }
-        </For>
-      </div>
-    )
-  }
-  return (
-    <button>{props.name}</button>
-
-  )
-}
 
 
 
@@ -64,14 +172,14 @@ function handleChange(index: number, event: Event & { currentTarget: HTMLInputEl
   setValues(newValues);
 }
 
-// async function handleGet() {
-//   let res = await axios.post(
-//     "http://localhost:9789/call", {
-//     "method": "retrieve",
-//     "address": address(),
-//     "params[]": ""
-//   }
-//   )
-//   console.log(res)
-//   setOutput(res.data)
-// }
+async function handleGet(obj: { method: string, address: string, params: string[] }) {
+  let res = await axios.post(
+    "http://localhost:9789/call", {
+    "method": obj.method,
+    "address": obj.address,
+    "params[]": obj.params
+  }
+  )
+  console.log(res)
+  setOutput(res.data)
+}
